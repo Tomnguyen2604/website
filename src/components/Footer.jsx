f3842e0dfb3ac339417bbc72dc2b77b420453a29
tomnguyen2604 <tomnguyen2604@gmail.com>
2025-04-28 23:39:33 -0400

    small update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Fade } from "react-awesome-reveal";
+
+const Footer = () => {
+    return (
+        <Fade>
+            <div id="footer">
+                <p>Build by Hoa Nguyen</p>
+                <p>All right reserve</p>
+            </div>
+        </Fade>
+    );
+};
