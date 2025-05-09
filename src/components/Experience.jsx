5908baf60cf301cc48350b8ccbf9ce5fac890955
tomnguyen2604 <tomnguyen2604@gmail.com>
2025-05-09 13:37:56 -0400

    small update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,13 +2,13 @@ import { Fade } from "react-awesome-reveal";
 
 const Expericance = () => {
     const tech_stack = [
-        { name: "Java", logo: "/logos/java.png" },
-        { name: "JavaScript", logo: "/logos/javascript.png" },
-        { name: "Python", logo: "/logos/python.png" },
-        { name: "React.js", logo: "/logos/react.png" },
-        { name: "Node.js", logo: "/logos/node.png" },
-        { name: "MySQL", logo: "/logos/mysql.png" },
-        { name: "MongoDB", logo: "/logos/mongodb.png" },
+        "Java",
+        "JavaScript",
+        "Python",
+        "React.js",
+        "Node.js",
+        "MySQL",
+        "MongoDB",
     ];
     return (
         <div className="experience-container">
@@ -21,21 +21,7 @@ const Expericance = () => {
                 <ul className="tech-stack">
                     {tech_stack.map((item, i) => (
                         <Fade key={i} triggerOnce duration={2000} delay={i * 5}>
-                            <li
-                                key={item.name}
-                                style={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    gap: "0.5rem",
-                                }}
-                            >
-                                <img
-                                    src={item.logo}
-                                    alt={item.name + " logo"}
-                                    style={{ width: "28px", height: "28px" }}
-                                />
-                                {item.name}
-                            </li>
+                            <li key={item}>{item}</li>
                         </Fade>
                     ))}
                 </ul>
