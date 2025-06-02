c648e936e27bb43e47140cc281f85ad010a9ca60
tomnguyen2604 <tomnguyen2604@gmail.com>
2025-06-02 10:55:42 -0400

    update contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
             <Fade triggerOnce>
                 <div className="contact_container">
                     <h1>Contact</h1>
-                    <p>Contact information will be displayed here.</p>
+                    <h2>Email: tomnguyen2604@gmail.com</h2>
                 </div>
             </Fade>
         </div>
