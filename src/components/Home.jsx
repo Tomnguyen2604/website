410fe2b6bf82d63c105d7a90477f3f51a66cf393
tomnguyen2604 <tomnguyen2604@gmail.com>
2025-06-25 23:45:33 -0400

    Update on resume button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,6 +23,12 @@ const Home = () => {
                     <h1>Hello, I'm Hoa Nguyen</h1>
                     <h2>Software Developer</h2>
                     <p>Welcome to my portfolio website</p>
+                    <a
+                        href="https://drive.google.com/file/d/1sQ3dpnXTIYwQZvBtT5Y1TeJu08TDrKET/view?usp=sharing"
+                        download
+                    >
+                        <button>Download Resume</button>
+                    </a>
                 </div>
             </Fade>
         </div>
